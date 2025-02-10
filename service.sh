23f8a6b81f7090ff864679110f05a44389550f85
史蒂夫ZMT工作室 <98326195+SteveZMTstudios@users.noreply.github.com>
2025-02-11 02:47:20 +0800

    make a rescue script

---
+++ service.sh
@@ -0,0 +1,10 @@
+MODDIR=${0%/*}
+sleep 120
+if [[ "`getprop init.svc.bootanim`" = "running" ]]; then
+echo > $MODDIR/disable
+
+echo "$(date '+%g-%m-%d %H:%M:%S') E detect: module bootlooped. Disable at next reboot. " > $MODDIR/status.log
+fi
+
+
+
