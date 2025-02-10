239151fda5e21ab76087aa81608185f241cf15f4
史蒂夫ZMT工作室 <98326195+SteveZMTstudios@users.noreply.github.com>
2025-02-11 02:45:06 +0800

    update config

--- README.md
+++ README.md
@@ -24,6 +24,28 @@ Enable Android Go on your low ram device by installing this module.
 
 安装后模块会移动配置。
 
+### 进阶配置
+
+模块默认**不启用**以下配置：
+
+- `debug.sf.latch_unsignaled`
+根据[android 文档](https://source.android.com/docs/core/graphics/unsignaled-buffer-latch?hl=zh-cn),`debug.sf.latch_unsignaled` 标志允许 SurfaceFlinger 锁存所有无信号的缓冲区,这在一些情况下会减少内存使用。但是根据官方文档声明，启用此配置会出现一些非预期的附带效应，例如在等待未完成的缓冲区期间破坏同步事务和冻结整个显示屏（虽然这从未发生在开发人员的设备上）。
+
+ - `debug.sf.nobootanimation`
+停用开机动画以省略开机动画载入和渲染流程，可能在部分设备上不生效。
+
+ - `persist.sys.ui.hw`
+ - `persist.sys.force_sw_gles`
+硬件加速的用户界面渲染。硬件加速可以提高图形性能和响应速度。
+
+禁用 JNI（Java Native Interface）检查。禁用这些检查可以提高性能，但可能会忽略一些潜在的 JNI 错误。
+
+禁用抖动效果。抖动是一种图像处理技术，用于减少颜色带状效应。禁用抖动可以提高性能，但可能会影响图像质量。
+
+清除资源的功能。启用这个功能可以帮助系统更有效地管理内存，特别是在资源有限的设备上。
+
+若要启用，请在/data/local/tmp/目录下新建enable文件，然后安装模块。
+
 ## 卸载说明
 
 1. 打开`Magisk Manager`应用。
@@ -31,12 +53,8 @@ Enable Android Go on your low ram device by installing this module.
 3. 找到您要卸载的模块，点击删除图标。
 4. 重启设备以完成卸载。
 
-
-
 ## 许可证
 
 本模块遵循GPLv3许可证。您可以在LICENSE文件中查阅完整的许可证文本。
 
-## 致谢
 
-感谢所有贡献者，以及感谢使用本模块。
--- customize.sh
+++ customize.sh
@@ -1,6 +1,13 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
+# 脚本版本 v1.2.19
 
+
+ui_print "- 通过修改build.prop来使系统启用低内存优化，同时启用了部分系统优化开关。"
+ui_print "  适用于安卓5以上内存不满1GB的设备。由于会严重破坏体验，内存超过2GB的设备不要刷入。"
+ui_print "  仍处于测试阶段，请确保你有救砖能力！ "
+ui_print "- 已内置基本救砖组件，卡开机120秒后自动禁用，但需要手动重启。"
+# 检查兼容性
 minSdk=21
 maxRam=2097152
 devRam=0
@@ -19,11 +26,46 @@ then
         touch $MODPATH/disable
         ui_print "! 你的设备上的内存足够大，不适用此模块，已将其禁用。"
     fi
-    ui_print "√ 配置完成！"
+    ui_print " "
+    ui_print "√ 检查完成！"
     ui_print "√ 由酷安@史蒂夫ZMT 开发"
 else
     ui_print "! 需要安卓5.0以及更高版本。"
     abort "! 模块未安装。"
+fi
+
+if [ -f "/data/local/tmp/enable" ];then
+    ui_print " "
+    ui_print " ==============================="
+    ui_print "- 发现/data/local/tmp/enable文件"
+    ui_print "- 正在应用激进的优化策略"
+    ui_print "- 可有效节省缓存内存，但是可能导致一些问题"
+    ui_print "  如屏幕死锁，后台进程消失，桌面进入卡顿等。"
+    ui_print "  责  任  自  负"
+    ui_print " ==============================="
+    ui_print " "
+    ui_print " "
+    echo "debug.sf.latch_unsignaled=1" >> $MODPATH/system.prop
+    echo "debug.sf.nobootanimation=1"  >> $MODPATH/system.prop
+    echo "persist.sys.ui.hw=1" >> $MODPATH/system.prop
+    echo "ro.kernel.checkjni=0" >> $MODPATH/system.prop
+    echo "ro.kernel.android.checkjni=0" >> $MODPATH/system.prop
+    echo "persist.sys.use_dithering=0" >> $MODPATH/system.prop
+    echo "persist.sys.purgeable_assets=1" >> $MODPATH/system.prop
+    echo "dalvik.vm.execution-mode=int:jit" >> $MODPATH/system.prop
+    rm /data/local/tmp/enable
+  else
+  ui_print " "
+  ui_print " ==============================="
+  ui_print "- 本模块有更激进的优化方案可用，可有效节省缓存内存，但是可能导致一些问题"
+  ui_print "  如屏幕死锁，后台进程消失，桌面进入卡顿等。"
+  ui_print "- 若要启用，请在/data/local/tmp/目录下新建enable文件"
+  ui_print "  ，然后重新使用Magisk Manager安装此模块。 "
+  ui_print " "
+  ui_print " ==============================="
+  ui_print " "
+  ui_print " "
+  sleep 3
 fi
 
 #发现彩蛋！
